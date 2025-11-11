7f5fbcbe40a05ff7705ae4879c88e36cc90fa638
Vipul Mehra <vipulmehra002@gmail.com>
2025-11-11 13:47:28 +0530

    update the cors file

--- src/main/java/com/paxaris/gateway/config/CorsConfig.java
+++ src/main/java/com/paxaris/gateway/config/CorsConfig.java
@@ -1,3 +1,5 @@
+package com.paxaris.gateway.config;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
@@ -7,7 +9,7 @@ import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 import java.util.List;
 
 @Configuration
-public class CorsGlobalConfig {
+public class CorsConfig {
 
     @Bean
     public CorsWebFilter corsWebFilter() {
